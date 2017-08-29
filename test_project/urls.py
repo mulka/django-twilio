aa9ef89ffe37251a4f0a5a80127a61353bbf8115
Kyle Mulka <kmulka@pawnguru.com>
2017-08-29 03:42:45 -0400

    fix url patterns for later versions of django

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+]
